8b0ac799c4677fb7a4004d39080400faac341bd2
Neha3010 <158296501+NehaBogoju@users.noreply.github.com>
2024-02-13 20:40:41 +0530

    Add files via upload

---
+++ Solution1.java
@@ -0,0 +1,19 @@
+package gfg;
+
+class Solution1
+{
+  int getMiddle(Node head)
+  {
+       // Your code here.
+       if(head==null)
+          return -1;
+      Node slow=head;
+      Node fast=head;
+      while(fast!=null && fast.next!=null){
+          slow=slow.next;
+          fast=fast.next.next;
+      }
+      
+      return slow.data;
+  }
+}
